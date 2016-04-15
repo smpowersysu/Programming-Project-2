3b8f5efc031e2a2d71eb97fb973e3a716c6a4203
chrisl3ach <cpleach@student.ysu.edu>
2016-04-15 18:18:08 -0400

    Added files via upload
    This is the basic code for the code39 segment.

---
+++ project2code39.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int x){
+    int y = x;
+    for(int n = 1; n < y; n++){
+        x = x*(y - n);
+    }
+    return x;
+}
+
+int main(){
+
+char a[9];
+
+cout << "Enter w or n: ";
+
+for (int i=0;i<9;i++){
+    cin >> a[i];}//Puts w or n into char array
+
+int x[3],z=0;
+
+for(int i=0;i<9;i++,z++){
+    if(a[i]=='w')
+    x[z]=8-i;//Fills int array with values 8-0 depending on w position
+    else
+    z=z-1;//Restricts making int array bigger than 3
+    }
+
+if(z!=3){//If z is not 3 too many or few wide bars entered
+    cout << "The number of wide bars is invalid";
+    return(1);}
+
+int k=4;//k runs from 4-2
+int q=0;//This is the decoded sum
+
+for(int i=0;i<3;i++,k--){//Sums
+    q=q+factorial(x[i])/(factorial(k)*factorial(x[i]-k));}
+
+cout <<"\n" << q;
+return 0;
+}
