be907d680b2806b11474f9650125e6e5c194f310
jcroach4 <jakecroach5@gmail.com>
2016-04-20 12:40:49 -0400

    Create Jake_Ryan_Brennen_code48.cpp

---
+++ Jake_Ryan_Brennen_code48.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string barcode;
+int factorial(int n);
+int choose(int n, int k);
+int x[4]; //puts the values of n into an array
+int z[4]; // values of k into an array
+int j; //place holder for the values of b[i]
+int f; // output value
+int k = 4; //values of k that decrement so that we can use them for z
+
+int main(){
+    for(int i=7; i>=0; i--){
+       cout << "Enter your barcode(b&w): ";
+        cin >> barcode[i];
+
+        if(barcode[i] == 'b'){
+            x[j] = i;
+            z[j] = k;
+            k--;
+            j++;
+        }
+    }
+    for(int i; i<4; i++){
+        if(x[i] >= z[i]){
+           f += choose(x[i], z[i]);
+        }
+        else{
+           f+= 0;
+        }
+    }
+    cout << f;
+}
+
+int choose(int n, int k){
+    int u;
+    int t = n-k;
+    //cout <<"called";
+    u = factorial(n)/(factorial(k)*factorial(t));
+    return u;
+}
+
+int factorial(int n){
+    for(int i = n-1; i>0; i--){
+        n = n*i;
+    }
+    if(n==0){
+        n=1;
+    }
+    return n;
+}
